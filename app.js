b73bafa08aad28fea84747a85400162320194d2d
Vino Aryo ACER <vinoaryo2000@gmail.com>
2023-07-26 15:49:52 +0700

    Changing Everything

--- app.js
+++ app.js
@@ -1,78 +1,22 @@
 const body = document.body;
-const themeIcon = document.querySelector("#themeButton i");
 
-function changeTitle(title) {
-  document.title = "VTech - " + title;
-}
+const themeBtn = document.getElementById("themeBtn");
+const themeBtnIcon = document.querySelector("#themeBtn i");
 
-function toggleSections(sectionName) {
-  // Get all section elements
-  const homeSection = document.querySelector(".home");
-  const aboutSection = document.querySelector(".about");
-  const contactSection = document.querySelector(".contact");
-
-  // Hide all section elements
-  homeSection.style.display = "none";
-  aboutSection.style.display = "none";
-  contactSection.style.display = "none";
-
-  // Show the selected section element
-  if (sectionName === "home") {
-    homeSection.style.display = "block";
-  } else if (sectionName === "about") {
-    aboutSection.style.display = "block";
-  } else if (sectionName === "contact") {
-    contactSection.style.display = "block";
-  }
-}
-
-if (body.dataset.theme === "light") {
-  themeIcon.className = "fa fa-moon";
-}
-
-if (body.dataset.theme === "dark") {
-  themeIcon.className = "fa fa-sun";
-}
-
-document.querySelector("#themeButton").addEventListener("click", () => {
-  if (body.dataset.theme === "light") {
+function changeTheme() {
+  if (themeBtnIcon.classList.contains("fa-sun")) {
     body.dataset.theme = "dark";
-    themeIcon.classList.replace("fa-moon", "fa-sun");
-    // themeIcon.classList.remove = 'fa fa-sun'
-    // themeIcon.classList.add = 'fa fa-moon'
+    localStorage.setItem("theme", "dark");
+    themeBtnIcon.classList.replace("fa-sun", "fa-moon");
   } else {
-    body.dataset.theme = "light";
-    themeIcon.classList.replace("fa-sun", "fa-moon");
-    // themeIcon.classList.remove = 'fa fa-moon'
-    // themeIcon.classList.add = 'fa fa-sun'
+    body.dataset.theme = "";
+    localStorage.setItem("theme", "light");
+    themeBtnIcon.classList.replace("fa-moon", "fa-sun");
   }
-});
+}
 
-// // Language href
-
-// document.querySelector("#php").addEventListener('click', () => {
-//   window.location.href = "https://www.php.net/";
-// })
-// document.querySelector("#laravel").addEventListener('click', () => {
-//   window.location.href = "https://www.laravel.com/";
-// })
-// document.querySelector("#node").addEventListener('click', () => {
-//   window.location.href = "https://www.nodejs.org/";
-// })
-// document.querySelector("#python").addEventListener('click', () => {
-//   window.location.href = "https://www.python.org/";
-// })
-// document.querySelector("#html").addEventListener('click', () => {
-//   window.location.href = "https://www.html.com/";
-// })
-
-// Splash Screen Logic
-
-const splash = document.querySelector(".splash");
-
-setTimeout(() => {
-  splash.style.opacity = 0;
-  setTimeout(() => {
-    splash.style.display = "none";
-  }, 1001);
-}, 500);
+if (localStorage.getItem("theme") === "dark") {
+  body.dataset.theme = "dark";
+} else {
+  body.dataset.theme = "";
+}
